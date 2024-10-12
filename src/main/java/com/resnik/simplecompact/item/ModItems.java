8619af0e39717e1a8103239761983885e0008304
Mike <mikeresnik@gmail.com>
2024-10-12 14:56:24 -0400

    Adds Compacting Gem.

---
+++ src/main/java/com/resnik/simplecompact/item/ModItems.java
@@ -0,0 +1,20 @@
+package com.resnik.simplecompact.item;
+
+import com.resnik.simplecompact.SimpleCompact;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(SimpleCompact.MODID);
+
+    public static final DeferredItem<Item> COMPACTING_GEM = ITEMS.register("compacting_gem",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus bus) {
+        ITEMS.register(bus);
+    }
+
+}
