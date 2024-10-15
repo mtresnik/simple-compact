b8c83d7ff4508db1d7407fbdc4e689dc7f752430
Mike <mikeresnik@gmail.com>
2024-10-15 18:58:15 -0400

    Adds glass smelting recipe for sugar blocks.

--- src/main/java/com/resnik/simplecompact/block/SugarBlock.java
+++ src/main/java/com/resnik/simplecompact/block/SugarBlock.java
@@ -1,28 +1,23 @@
 package com.resnik.simplecompact.block;
 
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.util.ColorRGBA;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.ColoredFallingBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.loot.LootParams;
-import net.minecraft.world.level.storage.loot.LootTable;
+import org.jetbrains.annotations.NotNull;
 
-import javax.annotation.Nullable;
 import java.util.List;
 
 public class SugarBlock extends ColoredFallingBlock {
-
-    @Nullable
-    protected ResourceKey<LootTable> drops;
 
     public SugarBlock(Properties properties) {
         super(new ColorRGBA(0xFF_FF_FF_FF), properties);
     }
 
     @Override
-    protected List<ItemStack> getDrops(BlockState state, LootParams.Builder params) {
+    protected @NotNull List<ItemStack> getDrops(BlockState state, LootParams.Builder params) {
         return List.of(new ItemStack(Items.SUGAR, 9));
     }
 
